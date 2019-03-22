0bac54239da1319c5297f690f50ce257771fa44c
uynet <highsate@gmail.com>
2019-03-23 03:38:28 +0900

    00:00:00:00

--- public/src/main.js
+++ public/src/main.js
@@ -15,8 +15,7 @@ let isPause = false;
 function Init(){
   gl = CreateGL();
   websocket = new Socket();
-  websocket.listen();
-
+  websocket.listen(); 
   world = new World();
   world.Init();
 }
@@ -50,7 +49,7 @@ function Clock(){
   //CPCTF終了19:30?
   let today = new Date();
   //today.setHours(19,30,0,0);
-  today.setHours(23,59,59,59);
+  today.setHours(23,59,59,99);
   let limit = today.getTime();
   let now = new Date().getTime();
   let ms= limit-now;
@@ -58,11 +57,16 @@ function Clock(){
   let mm = String(Math.floor((ms - hh * 3600000)/60000)+ 100).substring(1);
   let ss = String(Math.floor((ms - hh * 3600000 - mm * 60000)/1000)+ 100).substring(1);
   let sss= String(ms%1000+1000).substring(1,3);
+  //if(sss>95)ss=ss[0]+"-";
+  //if(sss>97)ss=ss[0]+"_";
+  if(ms<0){
+    document.getElementById("main").style.color = "#fd107a";
+    hh = mm = ss = sss = "00";
+  }
   let text =  "["
     +hh+":"+mm+":"+ss + ":" + sss
    +"]";
   document.getElementById("main").innerHTML = text;
-  //document.getElementById("main").style.color = "#ff0000";
 }
 //main loop
 function Run(){
