3e3e27c71c20925a1429b4ac3e15ff0aa31b08a0
shirodoni <jishoindoa@gmail.com>
2019-03-22 20:35:13 +0900

    テキストが常にカメラ方向を向く(一応)

--- public/src/entity/textbox.js
+++ public/src/entity/textbox.js
@@ -6,14 +6,20 @@ class TextBox{
     this.buffers;
     this.program = Material.GetProgram("text");
     this.onReady = false;
-    this.primitiveType = "TRIANGLES";
+    this.primitiveType = "ELEMENTS";
     this.Init();
   }
   async Init(){
     this.textTexture = await Material.CreateTextureByString(this.text);
 
     const vertices = SquareArray(1.0);
-    const uv = SquareUVArray();
+    //const uv = SquareUVArray();
+    const uv = [
+      0.0 , 0.0 ,
+      1.0 , 0.0 ,
+      0.0 , 1.0 ,
+      1.0 , 1.0 ,
+    ];
 
     this.buffers = [
       this.VBO = new Buffer(vertices,"position",3),
@@ -30,6 +36,17 @@ class TextBox{
     this.drawObject.AddUniform("viewMatrix","mat4",()=>{return world.mainCamera.GetViewMatrix()});
     this.drawObject.AddUniform("projMatrix","mat4",()=>{return world.mainCamera.GetProjMatrix()});
     this.drawObject.AddUniform("transformMatrix","mat4",()=>{return GetTransformMatrix(self.pos)});
+    this.drawObject.AddUniform("rotMatrix", "mat4", ()=>{
+      //ビュー変換行列の回転成分の逆行列(交代行列なので転置)を渡す。
+      //(ほんとに正しいのかこれ？)
+      const v = world.mainCamera.GetViewMatrix();
+      return [
+        v[0], v[4], v[8], 0,
+        v[1], v[5], v[9], 0,
+        v[2], v[6], v[10], 0,
+        0, 0, 0, 1
+      ];
+    });
     this.drawObject.AddUniform("trap","texture",()=>{return self.textTexture});
     this.onReady = true;
   };
