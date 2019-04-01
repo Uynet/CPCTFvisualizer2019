25280b4f867dea2eb8f09f2e6f9d14cf8cd9d43b
shirodoni <jishoindoa@gmail.com>
2019-04-01 20:02:54 +0900

    拡大処理, Enterを押すと出る

--- public/src/entity/ripple.js
+++ public/src/entity/ripple.js
@@ -1,13 +1,19 @@
 class Ripple {
-  constructor(pos) {
+  // 場所, 最大サイズ, 拡大時間, 拡大後生存時間
+  constructor(pos, maxSize = 1, expandFrame = 10, lastFrame = 5) {
     this.pos = pos;
+    this.maxSize = maxSize;
+    this.expandFrame = expandFrame;
+    this.lastFrame = lastFrame;
+
     this.program = Material.GetProgram("ripple");
-    
     this.primitiveType = "ELEMENTS";
 
     this.Init();
   }
   Init() {
+    this.frameCount = 0;
+
     const vertices = SquareArray(1);
     const uv = SquareUVArray();
     const idx = [0, 2, 1, 1, 2, 3];
@@ -24,11 +30,20 @@ class Ripple {
     this.drawObject.AddUniform("viewMatrix","mat4",()=>{return world.mainCamera.GetViewMatrix()});
     this.drawObject.AddUniform("projMatrix","mat4",()=>{return world.mainCamera.GetProjMatrix()});
     this.drawObject.AddUniform("transformMatrix","mat4",()=>{return GetTransformMatrix(this.pos)});
+    this.drawObject.AddUniform("size", "1f", ()=>{return this.getSize()});
+  }
+  getSize() {
+    return Math.min(1, Math.pow(this.frameCount / this.expandFrame, 2)) * this.maxSize;
   }
   Update() {
-
+    this.frameCount++;
+    if (this.frameCount === this.expandFrame + this.lastFrame) {
+      //消滅させる処理
+      console.log("消滅");
+    }
   }
   Draw() {
     this.drawObject.Draw();
   }
+
 }
